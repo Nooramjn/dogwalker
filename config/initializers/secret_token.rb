7ed6e4086adf195ae038821d4842ebcff0d5ef62
Nooramjn <nmslmani@gmail.com>
2012-11-21 16:02:42 +0300

    starting new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogwalker::Application.config.secret_token = 'd2671d997e828eea0cf42a2f421c1a191ab8b31d58b7e7392e433c3cdd9d6b208924e010517e58f71a6175b38ade14846a17340d9783fadb7d0f53baccbe3209'
